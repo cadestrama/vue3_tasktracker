22198a839d6db15cacf0dd1fc2fda3583332c953
Gary <garymsqd3@gmail.com>
2022-08-26 19:05:04 +0800

    adding_methods in app
    methods, watch, counter, followUser()

--- twotter-tutorial/src/App.vue
+++ twotter-tutorial/src/App.vue
@@ -1,16 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Twotter"/>
+
+  <div class="app">
+    @_{{user.username}}-{{fullName}}
+
+    <div>
+     <strong>followers: </strong> {{ followers}}
+    </div>
+
+
+    <button @click="followUser">Follow</button>
+
+  </div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data(){
+    return {
+      followers: 0,
+      user: {
+        id:1,
+        username: 'jdoe',
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@gmail.com',
+        isAdmin: true
+      }
+    }
+  },
+
+  watch: {
+    followers(newFollowerCount, oldFollowerCount){
+      if(oldFollowerCount < newFollowerCount){
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+  },
+
+  computed: {
+    fullName(){
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+
+  methods: {
+    followUser(){
+      this.followers++
+    }
+  },
+
+// loaded the app vue
+  mounted(){
+    this.followUser();
   }
+ 
 }
 </script>
 
@@ -19,8 +67,13 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
+
 }
 </style>
